1e353bfc6ff0086eabf0f45793ac107f1831e102
Esteban Saldarriaga Alzate <estebansa@outlook.com>
2021-09-21 19:21:48 -0500

    Features and enhancements:
    - Add create and new action, views and routes
    - Add validations for Article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,25 @@ class ArticlesController < ApplicationController
   def show
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: 'Article was created successfully.'
+    else
+      render :new
+    end
+  end
+
   private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
   def set_article
     @article = Article.find(params[:id])
