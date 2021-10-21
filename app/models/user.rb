1809f997f60672a9c6f363c16ba868c760142ae9
Esteban Saldarriaga Alzate <estebansa@outlook.com>
2021-10-20 19:43:22 -0500

    add has_secure_password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
     length: { minimum: 3, maximum: 25 }
   validates :email, presence: true,  uniqueness: { case_sensitive: false }, 
     length: { maximum: 105 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  before_save :downcase_email
+
+  has_secure_password
+
+  def downcase_email
+    email.downcase
+  end
 end
