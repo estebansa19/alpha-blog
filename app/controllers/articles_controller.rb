4f2226d7804c981cc75d61d39e8b601fca19564d
Esteban Saldarriaga Alzate <estebansa@outlook.com>
2021-10-20 19:14:45 -0500

    Add relation for users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
 
     if @article.save
       redirect_to @article, notice: 'Article was created successfully.'
